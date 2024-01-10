588ce04877d3e292b7c125e838b023d5349582d6
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-01-10 17:23:16 -0300

    style: css da estrutura do skeleton inicial skeleton

--- skeleton/style.ts
+++ skeleton/style.ts
@@ -6,7 +6,24 @@ export const styles = StyleSheet.create({
         backgroundColor: '#212121',
         padding: 32,
     },
-    txt1: {
-        color: 'white',
-    }
+    header: {
+        marginTop: 32,
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 12,
+    },
+    avatar: {
+        height: 64,
+        width: 64,
+        borderRadius: 32,
+    },
+    greeting: {
+        color: "#FFF",
+        fontSize: 14,
+    },
+    username: {
+        color: "#FFF",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
 });
