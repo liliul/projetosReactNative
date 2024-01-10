faffcd1d6e329869989b46aae064e6ab49b6bf7c
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-01-10 17:22:53 -0300

    create: estrutura do skeleton inicial skeleton

--- skeleton/App.tsx
+++ skeleton/App.tsx
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
 import { styles } from './style';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.txt1}>Testando Skeleton no react navite</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor='transparent' translucent />
+      <View style={styles.header}>
+        <Image
+          source={{ uri: "https://avatars.githubusercontent.com/u/32183622?v=4" }}
+          style={styles.avatar}
+        />
+
+        <View>
+          <Text style={styles.greeting}>Hello</Text>
+          <Text style={styles.username}>Liliul Github</Text>
+        </View>
+      </View>
     </View>
   );
 }
