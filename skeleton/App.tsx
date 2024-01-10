a044354094c42415050b7275528a890e5c4e7f23
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-01-10 17:44:32 -0300

    feat: ContentLoader skeleton

--- skeleton/App.tsx
+++ skeleton/App.tsx
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, useWindowDimensions } from 'react-native';
+import ContentLoader from 'react-content-loader';
 
 import { styles } from './style';
+import { Circle, Rect } from 'react-native-svg';
 
 export default function App() {
+  const { height, width } = useWindowDimensions();
   return (
     <View style={styles.container}>
       <StatusBar style="light" backgroundColor='transparent' translucent />
+
+      <ContentLoader
+        viewBox={`0 0 ${width} ${height}`}
+        backgroundColor='aquamarine'
+        foregroundColor='royalblue'
+      >
+        <Circle cx="32" cy="32" r="32" />
+        <Rect x="80" y="17" rx="4" ry="4" width={30} height={12} />
+        <Rect x="80" y="40" rx="4" ry="4" width={200} height={14} />
+      </ContentLoader>
+
       <View style={styles.header}>
         <Image
           source={{ uri: "https://avatars.githubusercontent.com/u/32183622?v=4" }}
