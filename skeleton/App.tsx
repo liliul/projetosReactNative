56ed271f2f3c11d60c2c449fffee9defa63cf788
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-01-10 18:06:32 -0300

    feat: isLoading skeleton

--- skeleton/App.tsx
+++ skeleton/App.tsx
@@ -4,34 +4,44 @@ import ContentLoader from 'react-content-loader';
 
 import { styles } from './style';
 import { Circle, Rect } from 'react-native-svg';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const { height, width } = useWindowDimensions();
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 3000);
+  }, [])
   return (
     <View style={styles.container}>
       <StatusBar style="light" backgroundColor='transparent' translucent />
 
-      <ContentLoader
-        viewBox={`0 0 ${width} ${height}`}
-        backgroundColor='aquamarine'
-        foregroundColor='royalblue'
-      >
-        <Circle cx="32" cy="32" r="32" />
-        <Rect x="80" y="17" rx="4" ry="4" width={30} height={12} />
-        <Rect x="80" y="40" rx="4" ry="4" width={200} height={14} />
-      </ContentLoader>
+      {isLoading ? (
+        <ContentLoader
+          viewBox={`0 0 ${width} ${height}`}
+          backgroundColor='aquamarine'
+          foregroundColor='royalblue'
+        >
+          <Circle cx="32" cy="32" r="32" />
+          <Rect x="80" y="17" rx="4" ry="4" width={30} height={12} />
+          <Rect x="80" y="40" rx="4" ry="4" width={200} height={14} />
+        </ContentLoader>
+      ) : (
 
-      <View style={styles.header}>
-        <Image
-          source={{ uri: "https://avatars.githubusercontent.com/u/32183622?v=4" }}
-          style={styles.avatar}
-        />
+        <View style={styles.header}>
+          <Image
+            source={{ uri: "https://avatars.githubusercontent.com/u/32183622?v=4" }}
+            style={styles.avatar}
+          />
 
-        <View>
-          <Text style={styles.greeting}>Hello</Text>
-          <Text style={styles.username}>Liliul Github</Text>
+          <View>
+            <Text style={styles.greeting}>Hello</Text>
+            <Text style={styles.username}>Liliul Github</Text>
+          </View>
         </View>
-      </View>
+      )}
+
     </View>
   );
 }
