49bf06db9ed72a557de9444f86d6c9aa0194598e
liliul <liliuestudo@gmail.com>
2024-01-18 17:02:22 -0300

    feat: infomacoes usuario no github createExpoStack

--- createExpoStack/components/githubApi.tsx
+++ createExpoStack/components/githubApi.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {View ,Text} from 'react-native'
 
 export default function githubInfos(props) {
+    const [github, setGithub] = useState('')
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/maykbrito')
+        .then((req) => req.json())
+        .then((res) => {
+            setGithub(res)
+        })
+    },[])
+
     return (
         <View>
             <Text className={styles.name}>{props.name}</Text>
+            <View>
+                <Text>ID: {github.id}</Text>
+                <Text>Name: {github.name}</Text>
+                <Text>Company: {github.company}</Text>
+                <Text>Location: {github.location}</Text>
+            </View>
         </View>
     )
 }
