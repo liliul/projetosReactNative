0cddaeb83f3fa14beff9fd27ae9cdede927eb5e7
liliul <liliuestudo@gmail.com>
2024-01-20 19:31:06 -0300

    fix: erro de syntax createExpoStack

--- createExpoStack/components/githubApi.tsx
+++ createExpoStack/components/githubApi.tsx
@@ -7,38 +7,40 @@ export default function githubInfos(props) {
     const [github, setGithub] = useState('')
     const [loading, setLoading] = useState(true)
 
-    setTimeout(() => {
-        useEffect(() => {
+    useEffect(() => {
         fetch('https://api.github.com/users/maykbrito')
         .then((req) => req.json())
         .then((res) => {
-            setGithub(res)
-            setLoading(false)
+            setTimeout(() => {
+                setLoading(false)
+                setGithub(res)
+            }, 1500)
         })
     },[])
-    }, 1500)
 
     return (
         <View style={styles.containerCard}>
-        {!loading
-            ? (<LoadGithubInfos loadingText={'Carregando Infos Do Github Usuario'  } />)
-            : (
-                 <Text style={styles.nameProps}>{props.name}</Text>
+            {loading
+                ? (<LoadGithubInfos loadingText='Carregando Infos Do Github Usuario' />)
+                : (
+                    <View>
+                        <Text style={styles.nameProps}>{props.name}</Text>
 
-                <View style={styles.gitContainer}>
-                    <Text>ID: {github.id}</Text>
-                    <Text>Name: {github.name}</Text>
-                    <Text>Company: {github.company}</Text>
-                    <Text>Location: {github.location}</Text>
-                </View>
+                        <View style={styles.gitContainer}>
+                            <Text>ID: {github.id}</Text>
+                            <Text>Name: {github.name}</Text>
+                            <Text>Company: {github.company}</Text>
+                            <Text>Location: {github.location}</Text>
+                        </View>
 
-                <View style={styles.gitContainer}>
-                    <Text>URL: {github.html_url}</Text>
-                    <Text>Repos URL: {github.repos_url}</Text>
-                    <Text>Bio: {github.bio}</Text>
-                </View>
-            )
-        }
+                        <View style={styles.gitContainer}>
+                            <Text>URL: {github.html_url}</Text>
+                            <Text>Repos URL: {github.repos_url}</Text>
+                            <Text>Bio: {github.bio}</Text>
+                        </View>
+                    </View>
+                )
+            }
         </View>
     )
 }
